9dfe5a7ab34fef774c8c9f99bb321728d19d772f
licon4812 <jalickolli@gmail.com>
2023-08-16 15:14:28 +1000

    Created Orleans.Host project template

---
+++ src/licon4812.Orleans.TemplatePack/templates/Orleans.ProjectTemplates/Orleans.Host/Program.cs
@@ -0,0 +1,28 @@
+﻿using Orleans.Providers;
+using Orleans.Runtime;
+
+namespace Orleans.Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.UseOrleans(siloBuilder =>
+            {
+                // Use localhost clustering by default
+                siloBuilder.UseLocalhostClustering();
+                // Setup memory as the grain storage - Add your own providers as required
+                siloBuilder.AddMemoryGrainStorageAsDefault();
+                // Setup memory streams - Add your own providers as required
+                siloBuilder.AddMemoryStreams<DefaultMemoryMessageBodySerializer>("MemoryStreamProvider");
+                // Enable streaming
+                siloBuilder.AddStreaming();
+            });
+
+            var app = builder.Build();
+            app.Run();
+        }
+    }
+}
